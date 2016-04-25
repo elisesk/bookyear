92f22af32169239113ab44c1991df744c0befd90
Elise Skaug Kjøndal <elise.skaug.kjondal@itera.no>
2016-04-25 09:14:39 +0200

    Updated index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 http.createServer(function (req, res) {
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/plain');
-	res.end('I am a Node app\n');
+	res.end('I am a Node app, made by the super talented unicorn Elise\n');
 }).listen(8080);
 
 console.log('Server hopefully running on port 8080..');
